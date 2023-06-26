7579c8a9892099ff8353c459945e1e5c18d3b78f
Deepanshu Gupta <deepanshu.gupta@collabera.com>
2023-06-26 20:11:03 +0530

    hash convert into string to bytes

--- models/block.go
+++ models/block.go
@@ -1,47 +1,49 @@
 package models
 
-import(
-	"time"
-	"fmt"
+import (
+	"crypto/sha256"
 	json "encoding/json"
+	"fmt"
+	"time"
 )
+
 // Its one block struct and that represent the block of our Blockchain
-type Block struct{
-	nonce int 
-	previousHash string
-	timestamp int64
+type Block struct {
+	nonce        int
+	previousHash [32]byte
+	timestamp    int64
 	transactions []*Transactions
 }
 
-func NewBlock(nonce int, previousHash string)*Block{
-	b:=new(Block)
-	b.timestamp=time.Now().UnixNano()
-	b.nonce=nonce
-	b.previousHash=previousHash
+func NewBlock(nonce int, previousHash [32]byte) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
 	return b
 }
-func (b *Block) Print(){
-	fmt.Printf("TimeStamp %d\n",b.timestamp)
-	fmt.Printf("nonce %d\n",b.nonce)
-	fmt.Printf("previous Hash %s\n",b.previousHash)
-	fmt.Printf("transactions %+v\n",b.transactions)
+func (b *Block) Print() {
+	fmt.Printf("TimeStamp %d\n", b.timestamp)
+	fmt.Printf("nonce %d\n", b.nonce)
+	fmt.Printf("previous Hash %s\n", b.previousHash)
+	fmt.Printf("transactions %+v\n", b.transactions)
 }
 
-func (b *Block)Hash() string{
-	m,_:=json.Marshal(b)
-	return string(m)
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
 }
 
-func (b *Block) MarshalJSON()([]byte,error){
-	return json.Marshal(struct{
-		TimeStamp int64 `json:"timestamp"`
-		Nonce int `json:"nonce"`
-		PreviousHash string `json:"previousHash"`
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		TimeStamp    int64           `json:"timestamp"`
+		Nonce        int             `json:"nonce"`
+		PreviousHash [32]byte        `json:"previousHash"`
 		Transactions []*Transactions `json:"transaction"`
 	}{
-		TimeStamp:b.timestamp,
-		Nonce:b.nonce,
-		Transactions:b.transactions,
-		PreviousHash:b.previousHash,
+		TimeStamp:    b.timestamp,
+		Nonce:        b.nonce,
+		Transactions: b.transactions,
+		PreviousHash: b.previousHash,
 	})
 }
--- models/chain.go
+++ models/chain.go
@@ -1,34 +1,38 @@
 package models
+
 import (
 	"fmt"
 	"strings"
 )
-type Chain struct{
+
+type Chain struct {
 	transactionsPool []*Transactions
-	chains []*Block
-}
-func NewChain() *Chain{
-	bc:=new(Chain)	
-	bc.CreateBlock(0,"intial block")
-	return bc
-}
-func (bc *Chain) CreateBlock(nonce int , previousHash string) *Block{
-	block:=NewBlock(nonce, previousHash)
-	bc.chains=append(bc.chains,block)
-	return block
-}
-func (bc * Chain) Print(){
-	for i, block:=range bc.chains{
-		fmt.Printf("%s chain %d %s\n",strings.Repeat("=",25),i, strings.Repeat("=",25))
-		block.Print()
-	}
-	fmt.Printf("%s\n",strings.Repeat("*",25))
+	chains           []*Block
 }
 
-func (bc *Chain) LastBlock() *Block{
+func NewChain() *Chain {
+	block := &Block{}
+	bc := new(Chain)
+	bc.CreateBlock(0, block.Hash())
+	return bc
+}
+func (bc *Chain) CreateBlock(nonce int, previousHash [32]byte) *Block {
+	block := NewBlock(nonce, previousHash)
+	bc.chains = append(bc.chains, block)
+	return block
+}
+func (bc *Chain) Print() {
+	for i, block := range bc.chains {
+		fmt.Printf("%s chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
+func (bc *Chain) LastBlock() *Block {
 	return bc.chains[len(bc.chains)-1]
 }
-func (bc *Chain) AddTransaction(senderAddress,ReceiverAddress string, value float64) {
-	transaction:=NewTransaction(senderAddress,ReceiverAddress,value)
-	bc.transactionsPool=append(bc.transactionsPool,transaction)
+func (bc *Chain) AddTransaction(senderAddress, ReceiverAddress string, value float64) {
+	transaction := NewTransaction(senderAddress, ReceiverAddress, value)
+	bc.transactionsPool = append(bc.transactionsPool, transaction)
 }

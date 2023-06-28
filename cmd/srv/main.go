d4bd0fdd56d0a40c06f7239523f17904633e6646
Deepanshu Gupta <deepanshu.gupta@collabera.com>
2023-06-28 21:44:31 +0530

    checking Total amount of particular wallet

--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"git.deep.block/models"
@@ -10,20 +11,34 @@ func init() {
 	log.SetPrefix("BlockChain:")
 }
 func main() {
-	// myBlockChainAddress := "myBlockChainAddress"
-	blockChain := models.NewChain(models.MINING_SENDER)
+	myBlockChainAddress := "myBlockChainAddress"
+	blockChain := models.NewChain(myBlockChainAddress)
 	blockChain.Print()
 
+	// blockChain.AddTransaction("A", "B", 1.0)
+	// previousHash := blockChain.LastBlock().Hash()
+	// nonce := blockChain.ProofOfWork()
+	// blockChain.CreateBlock(nonce, previousHash)
+	// blockChain.Mining()
+	// blockChain.Print()
+
+	// blockChain.AddTransaction("C", "D", 2.0)
+	// blockChain.AddTransaction("X", "Y", 10.0)
+	// blockChain.Mining()
+	// previousHash = blockChain.LastBlock().Hash()
+	// nonce = blockChain.ProofOfWork()
+	// blockChain.CreateBlock(nonce, previousHash)
+	// blockChain.Print()
+
 	blockChain.AddTransaction("A", "B", 1.0)
-	previousHash := blockChain.LastBlock().Hash()
-	nonce := blockChain.ProofOfWork()
-	blockChain.CreateBlock(nonce, previousHash)
+	blockChain.Mining()
 	blockChain.Print()
 
 	blockChain.AddTransaction("C", "D", 2.0)
 	blockChain.AddTransaction("X", "Y", 3.0)
-	previousHash = blockChain.LastBlock().Hash()
-	nonce = blockChain.ProofOfWork()
-	blockChain.CreateBlock(nonce, previousHash)
+	blockChain.Mining()
 	blockChain.Print()
+	fmt.Printf("my %.1f\n", blockChain.CalculatTotalAmount(myBlockChainAddress))
+	fmt.Printf("C %.1f\n", blockChain.CalculatTotalAmount("C"))
+	fmt.Printf("D %.1f\n", blockChain.CalculatTotalAmount("D"))
 }

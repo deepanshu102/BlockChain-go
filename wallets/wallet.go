9fd1c1f6ddc71eb88fdc737792537be0be27930d
Deepanshu Gupta <deepanshu.gupta@collabera.com>
2023-07-04 15:59:04 +0530

    Wallet code done without Blockchain address'

--- wallets/wallet.go
+++ wallets/wallet.go
@@ -4,6 +4,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"fmt"
 )
 
 type Wallet struct {
@@ -11,11 +12,34 @@ type Wallet struct {
 	publicKey  *ecdsa.PublicKey
 }
 
+/*
+use for create a New Wallet
+*/
 func NewWallet() *Wallet {
 	w := new(Wallet)
 	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	w.privateKey = privateKey
 	w.publicKey = &w.privateKey.PublicKey
 	return w
+}
 
+// PrivateKey() is
+// use for get access of private key of particular wallet
+func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
+	return w.privateKey
+}
+
+// PrivateKeyStr() is help to get Private key in readable format
+func (w *Wallet) PrivateKeyStr() string {
+	return fmt.Sprintf("%x", w.privateKey.D.Bytes())
+}
+
+// PublicKey() is used for get details of PublicKey of particular wallet
+func (w *Wallet) PublicKey() *ecdsa.PublicKey {
+	return w.publicKey
+}
+
+// PublicKeyStr() is hepl to get Public key in readable format
+func (w *Wallet) PublicKeyStr() string {
+	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
 }

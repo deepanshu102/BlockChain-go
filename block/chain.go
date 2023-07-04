a1b599c9714fc5527c438c6b66caac079fcce11e
Deepanshu Gupta <deepanshu.gupta@collabera.com>
2023-07-04 16:07:56 +0530

    wallet introduce

--- block/chain.go
+++ block/chain.go
@@ -1,9 +1,12 @@
-package models
+package block
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
+	"time"
 )
 
 var (
@@ -11,6 +14,85 @@ var (
 	MINING_SENDER     = "Miner Address"
 	MINING_REWARD     = 1.0
 )
+
+// Its one block struct and that represent the block of our Blockchain
+type Block struct {
+	nonce        int
+	previousHash [32]byte
+	timestamp    int64
+	transactions []*Transactions
+}
+
+func NewBlock(nonce int, previousHash [32]byte, transactions []*Transactions) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	b.transactions = transactions
+	return b
+}
+func (b *Block) Print() {
+	fmt.Printf("TimeStamp %d\n", b.timestamp)
+	fmt.Printf("nonce %d\n", b.nonce)
+	fmt.Printf("previous Hash %x\n", b.previousHash)
+	for _, t := range b.transactions {
+		t.Print()
+	}
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
+}
+
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		TimeStamp    int64           `json:"timestamp"`
+		Nonce        int             `json:"nonce"`
+		PreviousHash [32]byte        `json:"previousHash"`
+		Transaction  []*Transactions `json:"transactions"`
+	}{
+		TimeStamp:    b.timestamp,
+		Nonce:        b.nonce,
+		Transaction:  b.transactions,
+		PreviousHash: b.previousHash,
+	})
+}
+
+type Transactions struct {
+	senderBlockChainAddress   string
+	receiverBlockChainAddress string
+	amount                    float64
+}
+
+func NewTransaction(senderAddress, receiverAddress string, amount float64) *Transactions {
+	return &Transactions{
+		senderBlockChainAddress:   senderAddress,
+		receiverBlockChainAddress: receiverAddress,
+		amount:                    amount,
+	}
+
+}
+
+func (t *Transactions) Print() {
+	fmt.Printf("%s\n", strings.Repeat("-", 40))
+	fmt.Printf("Sender Address: %s\n", t.senderBlockChainAddress)
+	fmt.Printf("Receiver Address %s\n", t.receiverBlockChainAddress)
+	fmt.Printf("Value : %.1f\n", t.amount)
+}
+
+func (t *Transactions) MarshalJSON() ([]byte, error) {
+	return json.Marshal(
+		struct {
+			SenderBlockChainAddress   string  `json:"senderBlockChainAddress"`
+			ReceiverBlockChainAddress string  `json:"receiverBlockchainAddress"`
+			Amount                    float64 `json:"amount"`
+		}{
+			SenderBlockChainAddress:   t.senderBlockChainAddress,
+			ReceiverBlockChainAddress: t.receiverBlockChainAddress,
+			Amount:                    t.amount,
+		})
+}
 
 type Chain struct {
 	transactionsPool  []*Transactions
--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"log"
 
-	"git.deep.block/models"
+	"git.deep.block/wallets"
 )
 
 func init() {
 	log.SetPrefix("BlockChain:")
 }
 func main() {
-	myBlockChainAddress := "myBlockChainAddress"
-	blockChain := models.NewChain(myBlockChainAddress)
-	blockChain.Print()
+	// myBlockChainAddress := "myBlockChainAddress"
+	// blockChain := models.NewChain(myBlockChainAddress)
+	// blockChain.Print()
 
 	// blockChain.AddTransaction("A", "B", 1.0)
 	// previousHash := blockChain.LastBlock().Hash()
@@ -30,15 +30,21 @@ func main() {
 	// blockChain.CreateBlock(nonce, previousHash)
 	// blockChain.Print()
 
-	blockChain.AddTransaction("A", "B", 1.0)
-	blockChain.Mining()
-	blockChain.Print()
+	// blockChain.AddTransaction("A", "B", 1.0)
+	// blockChain.Mining()
+	// blockChain.Print()
 
-	blockChain.AddTransaction("C", "D", 2.0)
-	blockChain.AddTransaction("X", "Y", 3.0)
-	blockChain.Mining()
-	blockChain.Print()
-	fmt.Printf("my %.1f\n", blockChain.CalculatTotalAmount(myBlockChainAddress))
-	fmt.Printf("C %.1f\n", blockChain.CalculatTotalAmount("C"))
-	fmt.Printf("D %.1f\n", blockChain.CalculatTotalAmount("D"))
+	// blockChain.AddTransaction("C", "D", 2.0)
+	// blockChain.AddTransaction("X", "Y", 3.0)
+	// blockChain.Mining()
+	// blockChain.Print()
+	// fmt.Printf("my %.1f\n", blockChain.CalculatTotalAmount(myBlockChainAddress))
+	// fmt.Printf("C %.1f\n", blockChain.CalculatTotalAmount("C"))
+	// fmt.Printf("D %.1f\n", blockChain.CalculatTotalAmount("D"))
+
+	w := wallets.NewWallet()
+	fmt.Printf("%+v\n", w.PrivateKey())
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Printf("%+v\n", w.PublicKey())
+	fmt.Println(w.PublicKeyStr())
 }
